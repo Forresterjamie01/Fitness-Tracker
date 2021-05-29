762637c9102ed53979c6098ee76772ef814f6697
Forresterjamie01 <Forresterjamie01@yahoo.com>
2021-05-29 01:06:08 -0400

    updating server data.

--- server.js
+++ server.js
@@ -1,20 +1,24 @@
 const express = require("express");
-const logger = require("morgan");
-const mongoose = require("mongoose");
+const app = express();
+const bodyParser = require('body-parser');
+
+const cors = require("cors")
+
+const mongoose = require("mongoose"); 
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/userdb", { useNewUrlParser: true });
+
+app.use(cors());
 
 const PORT = process.env.PORT || 3000;
 
-const User = require("./models/userWorkout");
-const app = express();
-
-app.use(logger("dev"));
+const User = require("./models/userworkout");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/userdb", { useNewUrlParser: true });
 
 app.post("/submit", ({ body }, res) => {
   User.create(body)
