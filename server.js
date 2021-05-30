c5e2746a697b82375b5651746eabc22b72d7301b
Forresterjamie01 <Forresterjamie01@yahoo.com>
2021-05-29 22:37:26 -0400

    updating connection mongo in the server.

--- server.js
+++ server.js
@@ -2,24 +2,37 @@ const express = require("express");
 const mongoose = require("mongoose");
 const logger = require("morgan");
 const path = require("path");
-
+const cors = require('cors');
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+require('dotenv').config();
+
+
+
+// Routes
+
 app.use(express.urlencoded({ extended: true }));
+
 app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
+    );
+    const connection = mongoose.connection;
+    connection.once('open', () => {
+      console.log("MongoDB database connection established successfully");
+    })
 
 app.use(express.static("public"));
 
 app.use(logger("dev"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness", {
-  useNewUrlParser: true,
-});
+app.use(cors());
 
-// Routes 
 app.use(require("./routes/apiroute.js"));
 
 app.use(require("./routes/userworkoutroute.js"));
@@ -28,6 +41,7 @@ app.get('/', function(req, res){
     console.log("Root Route")
     res.json({ message: "hello world" });
 });
+
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
