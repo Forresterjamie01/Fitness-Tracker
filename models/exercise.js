adb351f7adbb360525dfcfceafd9c35164692ff1
Forresterjamie01 <Forresterjamie01@yahoo.com>
2021-06-01 22:52:28 -0400

    updated models.

---
+++ models/exercise.js
@@ -0,0 +1,58 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const exerciseSchema = new Schema({
+//user model to store user data
+   day:{
+       type:Date,
+       default: () => new Date()
+   },
+   excercises:[
+       {
+           type: {
+               type:String,
+               required:true
+           },
+
+           name: {
+            type:String,
+            required:true
+        },
+
+        distance: {
+            type:Number,
+            required:true
+        },
+
+        duration: {
+            type:Number,
+            required:true
+        },
+
+        weight: {
+            type:Number,
+            required:true
+        },
+
+        sets: {
+            type:Number,
+            required:true
+        },
+
+        reps: {
+            type:Number,
+            required:true
+        }
+
+       }
+   ]
+   
+
+}, {
+    timestamps: true,
+});
+
+const Exercise = mongoose.model("Exercise", exerciseSchema);
+
+module.exports = Exercise;
